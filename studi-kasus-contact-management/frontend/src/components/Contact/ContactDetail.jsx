218e01e73d5be0dde0e23ac9b4c1a4b3425c8f9d
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-10-18 20:12:21 +0700

    fixed BUG

--- studi-kasus-contact-management/frontend/src/components/Contact/ContactDetail.jsx
+++ studi-kasus-contact-management/frontend/src/components/Contact/ContactDetail.jsx
@@ -133,8 +133,8 @@ export default function ContactDetail() {
               {/* address card */}
               {addresses.map(address => (
                 <div
-                  key="{address.id}"
-                  className="bg-gray-700 bg-opacity-50 p-5 rounded-lg shadow-md border border-gray-600 card-hover"
+                  key={address.id}
+                  className="bg-gray-700 bg-opacity-50 p-5 rounded-lg shadow-md border border-gray-600 card-hover mb-3"
                 >
                   <div className="flex items-center mb-3">
                     <div className="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center mr-3 shadow-md">
